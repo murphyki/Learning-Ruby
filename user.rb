5978369c71798e4e3a952e76c50053c59b2771fc
kieran <kierantmurphy@gmail.com>
2014-06-30 00:47:40 +0100

    updated examples

--- user.rb
+++ user.rb
@@ -1,2 +1,5 @@
 class User
+  def in_role?(role)
+    true
+  end
 end
--- user_spec.rb
+++ user_spec.rb
@@ -1,5 +1,9 @@
 require 'user'
 
 describe User do
+  it "should be in any roles assigned to it" do
+    user = User.new
+    expect(user).to be_in_role("assigned role")
+  end
 end
 
